9ef06bd4379a26efaaffab999307e1d134d69ab4
saddaul-siam <saddadul.siam@gmail.com>
2021-10-27 12:14:00 +0600

    added firebase config

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA9qk2znh5syTkvumNurOHHAyXt6UgqIHE",
-    authDomain: "genius-car-mechanics-24.firebaseapp.com",
-    projectId: "genius-car-mechanics-24",
-    storageBucket: "genius-car-mechanics-24.appspot.com",
-    messagingSenderId: "25018901629",
-    appId: "1:25018901629:web:5893c2c06d2de612eabd14"
+    apiKey: "AIzaSyDGR4waSKLy98_4pg2mtd-EHLxW-oagMvo",
+    authDomain: "genius-car-mechanics-24-11710.firebaseapp.com",
+    projectId: "genius-car-mechanics-24-11710",
+    storageBucket: "genius-car-mechanics-24-11710.appspot.com",
+    messagingSenderId: "637080048839",
+    appId: "1:637080048839:web:fbba25640d4af6a4515ae2"
 };
 
 export default firebaseConfig;
